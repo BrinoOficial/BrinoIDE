5539bf17cea1ed74293880f75242231bfec0bf05
Mateus Berardo <mateus.berardo@brino.cc>
2018-02-21 11:18:21 -0300

    criando compiler

---
+++ src/Compiler.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Br.ino Qt DestaqueSintaxe
+
+Codigo de destaque da sintaxe do BRpp da IDE Br.ino
+em PyQt4 (python 2.7)
+
+    IDE do Br.ino  Copyright (C) 2018  Br.ino
+
+    Este arquivo e parte da IDE do Br.ino.
+
+    A IDE do Br.ino e um software livre: voce pode redistribui-lo
+    e / ou modifica-lo de acordo com os termos da Licenca Publica
+    Geral GNU, conforme publicado pela Free Software Foundation,
+    seja a versao 3 da Licenca , ou (na sua opcao) qualquer
+    versao posterior.
+
+    A IDE do Br.ino e distribuida na esperanca de que seja util,
+    mas SEM QUALQUER GARANTIA; sem a garantia implicita de
+    COMERCIALIZACAO ou ADEQUACAO A UM DETERMINADO PROPOSITO.
+    Consulte a Licenca Publica Geral GNU para obter mais detalhes.
+
+    Voce deveria ter recebido uma copia da Licenca Publica Geral
+    GNU junto com este programa. Caso contrario, veja
+    <https://www.gnu.org/licenses/>
+
+website: brino.cc
+author: Mateus Berardo
+email: mateus.berardo@brino.cc
+contributor: Victor Rodrigues Pacheco
+email: victor.pacheco@brino.cc
+"""
+
+import os
+
+
+def compilar():
+    cmd = ""
+    cmd += os.path.join('builder', 'arduino-builder')
+    cmd += " -compile "
+    cmd += " -hardware " + os.path.join('builder', 'hardware')
+    cmd += " -hardware " + os.path.join('builder', '.arduino15', 'packages')
