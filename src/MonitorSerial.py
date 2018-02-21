d2a96c15757e6bcb928a8fce90fb093453c1c7dd
Victor Pacheco <granix3@gmail.com>
2018-02-21 17:26:28 -0300

    Monitor serial front-end

--- src/MonitorSerial.py
+++ src/MonitorSerial.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QTextEdit, QWidget, QVBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPlainTextEdit, QLineEdit, QPushButton, QCheckBox
 
 
 """
@@ -35,9 +35,9 @@ contributor: Victor Rodrigues Pacheco
 email: victor.pacheco@brino.cc
 """
 
+
 def monitor_serial():
     print "Abrindo serial"
-    monitor_serial = MonitorSerial()
     return monitor_serial
 
 
@@ -48,14 +48,32 @@ class MonitorSerial(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.setGeometry(600, 50, 300, 300)
+        self.setGeometry(650, 50, 400, 500)
         layout = QGridLayout(self)
-        layout.setRowStretch(0, 7.5)
-        layout.setRowStretch(1, 2.5)
-        layout.setColumnMinimumWidth(0, 60)
-        layout.setSpacing(5)
-        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setColumnStretch(0, 8)
+        layout.setColumnStretch(1, 2)
+        layout.setRowStretch(0, 1)
+        layout.setRowStretch(1, 12)
+        layout.setRowStretch(2, 1)
+        self.setStyleSheet("background:#252525")
         self.setWindowTitle('Monitor Serial')
-        textEdit = QTextEdit()
-        layout = QVBoxLayout()
-        layout.addWidget(textEdit)
+
+        linha_envio = QLineEdit(self)
+        linha_envio.setStyleSheet("border-radius:3px;background:#101010;margin-bottom:2px;margin-right:2px;")
+
+        log_monitor = QPlainTextEdit(self)
+        log_monitor.setDisabled(True)
+        log_monitor.setStyleSheet("border-radius:3px;background:#101010;margin-bottom:2px;margin-right:2px;")
+
+        btn_enviar = QPushButton("Enviar")
+        btn_enviar.setObjectName("btn_enviar_name")
+        btn_enviar.setStyleSheet("""#btn_enviar_name{border-radius:2px;color:#252525;background:#5cb50d;margin:2px;}
+                                    #btn_enviar_name:hover{border:1px solid #5cb50d;color:#5cb50d;background:#252525}""")
+
+        rolagem_check = QCheckBox(self)
+        rolagem_check.setText("Rolagem-automática")
+
+        layout.addWidget(linha_envio, 0, 0)
+        layout.addWidget(log_monitor, 1, 0, 1, 0)
+        layout.addWidget(btn_enviar, 0, 1)
+        layout.addWidget(rolagem_check, 2, 0)
