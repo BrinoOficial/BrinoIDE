3c541d8c33ec7450898eac0e8dccc45eabbde301
Victor Pacheco <granix3@gmail.com>
2018-03-01 17:02:13 -0300

    fixes

--- src/EditorDeTexto.py
+++ src/EditorDeTexto.py
@@ -202,18 +202,7 @@ class Achar(QWidget):
         self.editor_de_codigo = editor
 
     def initUI(self):
-        lbl1 = QLabel('Zetcode', self)
-        lbl1.move(15, 10)
-
-        lbl2 = QLabel('tutorials', self)
-        lbl2.move(35, 40)
-
-        lbl3 = QLabel('for programmers', self)
-        lbl3.move(55, 70)
-
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Absolute')
-        self.show()
+        pass
 
     def sizeHint(self):
         return QSize(0, 20)
