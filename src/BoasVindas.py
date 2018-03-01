8450229c11cb49e53bf6a3ff792161c8f157ed83
Mateus Berardo <mateus.berardo@brino.cc>
2018-03-01 13:30:22 -0300

    boas vindas

---
+++ src/BoasVindas.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+
+class BoasVindas(QWidget):
+    def __init__(self):
+        super(BoasVindas, self).__init__()
+        layout = QVBoxLayout(self)
