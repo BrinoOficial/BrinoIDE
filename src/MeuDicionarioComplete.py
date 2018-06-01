93510d4a6e12fca70ea7cdbc9c627a93a68db65e
Mateus Berardo <mateus.berardo@brino.cc>
2018-06-01 10:44:38 -0300

    Auto-complete

--- src/MeuDicionarioComplete.py
+++ src/MeuDicionarioComplete.py
@@ -9,14 +9,14 @@ date: 13/05/2018
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QCompleter
 
-from GerenciadorDeKeywords import get_highlights
+from GerenciadorDeKeywords import get_words
 
 
 class MeuDicionarioComplete(QCompleter):
     insertText = pyqtSignal(str)
 
     def __init__(self, minhas_palavras_chaves=None, parent=None):
-        minhas_palavras_chaves = get_highlights('1') + get_highlights('2') + get_highlights('3') + get_highlights('4')
+        minhas_palavras_chaves = get_words('1') + get_words('2') + get_words('3') + get_words('4')
         minhas_palavras_chaves = sorted(minhas_palavras_chaves)
         QCompleter.__init__(self, minhas_palavras_chaves, parent)
         self.activated.connect(self.changeCompletion)
