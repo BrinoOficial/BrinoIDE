25c3bf56be9a776af51bb98ee0753bdce7804629
Victor Pacheco <granix3@gmail.com>
2019-02-09 18:21:55 -0200

    Box Abrir no "Bias vindas" adicionado

--- src/BoasVindas.py
+++ src/BoasVindas.py
@@ -69,10 +69,13 @@ class BoasVindas(QWidget):
         """
         novo = Item(self, self.parent.nova_aba, "Rascunho em Branco", "Crie um novo rascunho do zero",
                     os.path.join('recursos', 'novoArquivo.png'))
+        abrir = Item(self, self.parent.abrir, "Abrir",
+                             "Abra um código local", "novoArquivo.png")
         aprender_mais = Item(self, self.abrir_site, "Não sabe como começar?",
                              "Visite nosso site para ler um de nossos tutoriais!", "")
         self.layout.addLayout(self.itens, 0, 0)
         self.itens.addWidget(novo)
+        self.itens.addWidget(abrir)
         self.itens.addWidget(aprender_mais)
         self.setFixedHeight(300)
 
